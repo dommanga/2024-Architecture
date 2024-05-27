0dedb69e7f43ac5510f99e541366b0d0bd1b22a8
Jimi <leejm21@postech.ac.kr>
2024-05-27 16:43:28 +0900

    direct-mapped cache.

--- Lab5/Cache.v
+++ Lab5/Cache.v
@@ -8,10 +8,10 @@
 
 
 module Cache #(parameter LINE_SIZE = 16,
-               parameter NUM_SETS = 8,
-               parameter NUM_WAYS = 2,
-               parameter TAG_SIZE = 25,
-               parameter IDX_SIZE = 3,
+               parameter NUM_WAYS = 1,
+               parameter TAG_SIZE = 24,
+               parameter IDX_SIZE = 4,
+               parameter NUM_SETS = 2 ** IDX_SIZE,
                parameter B_OFFSET = 2) (
     input reset,
     input clk,
@@ -34,8 +34,8 @@ module Cache #(parameter LINE_SIZE = 16,
   wire [3:0] clog2 = `CLOG2(LINE_SIZE);
   wire [LINE_SIZE * 8 - 1:0] dout_dmem;
 
-  wire [TAG_SIZE - 1:0] tag = addr[31:7];
-  wire [IDX_SIZE - 1:0] idx = addr[6:4];
+  wire [TAG_SIZE - 1:0] tag = addr[31:IDX_SIZE+4];
+  wire [IDX_SIZE - 1:0] idx = addr[IDX_SIZE+4-1:4];
   wire [B_OFFSET - 1:0] bo = addr[3:2];
   wire [LINE_SIZE * 8 - 1:0] data_BUS;
   wire hit_way; // will be used only when is_hit is true.
@@ -51,18 +51,17 @@ module Cache #(parameter LINE_SIZE = 16,
   reg is_dirty[0:NUM_SETS - 1] [0:NUM_WAYS - 1];
   reg [LINE_SIZE * 8 - 1:0] DataBank[0:NUM_SETS - 1] [0:NUM_WAYS - 1];
 
-  reg LRU[0:NUM_SETS - 1];
-  reg flip_LRU, cache_write, is_dmem_input_valid, dmem_read, dmem_write, cache_alloc, cache_clean;
+  // reg LRU[0:NUM_SETS - 1];
+  reg cache_write, is_dmem_input_valid, dmem_read, dmem_write, cache_alloc, cache_clean;
 
   // assignments
   assign is_ready = (next_state == `S_IDLE);
   assign is_output_valid = (current_state == `S_CMP); // we have valid output (hit or miss info)
   assign is_hit =
-  (tag == TagBank[idx][0] && is_valid[idx][0]) ||
-  (tag == TagBank[idx][1] && is_valid[idx][1]);
-  assign hit_way = (tag == TagBank[idx][1]);
+  (tag == TagBank[idx][0] && is_valid[idx][0]);
+  // assign hit_way = (tag == TagBank[idx][1]);
 
-  assign data_BUS = (tag == TagBank[idx][0]) ? DataBank[idx][0] : DataBank[idx][1];
+  assign data_BUS = DataBank[idx][0];
 
   mux_4_to_1 bo_mux (
     .A (data_BUS[31:0]),    // input
@@ -85,7 +84,7 @@ module Cache #(parameter LINE_SIZE = 16,
         if (is_hit)
           next_state = `S_IDLE;
         else // miss
-          if (is_dirty[idx][LRU[idx]])
+          if (is_dirty[idx][0])
             next_state = `S_WB;
           else
             next_state = `S_ALLOC;
@@ -108,7 +107,7 @@ module Cache #(parameter LINE_SIZE = 16,
   always @(*) begin
 
     // initialize control signals.
-    flip_LRU = 0;
+    // flip_LRU = 0;
     cache_write = 0;
     is_dmem_input_valid = 0;
     cache_alloc = 0;
@@ -118,8 +117,8 @@ module Cache #(parameter LINE_SIZE = 16,
 
       `S_CMP: begin
         if (is_hit) begin
-          if (hit_way == LRU[idx])
-            flip_LRU = 1;
+          // if (hit_way == LRU[idx])
+          //   flip_LRU = 1;
           
           if (mem_rw == 1) // write
             cache_write = 1;
@@ -127,10 +126,10 @@ module Cache #(parameter LINE_SIZE = 16,
         else begin // miss - need to set input information to use DMEM
           if (next_state == `S_WB) begin
               is_dmem_input_valid = 1;
-              addr_dmem = {TagBank[idx][LRU[idx]], idx,2'b00, 2'b00};
+              addr_dmem = {TagBank[idx][0], idx,2'b00, 2'b00};
               dmem_read = 0;
               dmem_write = 1;
-              din_dmem = DataBank[idx][LRU[idx]];
+              din_dmem = DataBank[idx][0];
           end
           else begin // next state == `S_ALLOC
             is_dmem_input_valid = 1;
@@ -159,7 +158,7 @@ module Cache #(parameter LINE_SIZE = 16,
       end
 
       default: begin
-        flip_LRU = 0;
+        // flip_LRU = 0;
         cache_write = 0;
         is_dmem_input_valid = 0;
         cache_alloc = 0;
@@ -182,48 +181,50 @@ module Cache #(parameter LINE_SIZE = 16,
     if (reset) begin
       for (i = 0; i < NUM_SETS; i = i + 1) begin
             /* verilator lint_off BLKSEQ */
-            TagBank[i][0] = 25'b0;
-            TagBank[i][1] = 25'b0;
+
+            TagBank[i][0] = 24'b0;
+            // TagBank[i][1] = 25'b0;
 
             is_valid[i][0] = 0;
-            is_valid[i][1] = 0;
+            // is_valid[i][1] = 0;
 
             is_dirty[i][0] = 0;
-            is_dirty[i][1] = 0;
+            // is_dirty[i][1] = 0;
 
             DataBank[i][0] = 128'b0;
-            DataBank[i][1] = 128'b0;
+            // DataBank[i][1] = 128'b0;
             
-            LRU[i] = 0;
+            // LRU[i] = 0;
+
             /* verilator lint_on BLKSEQ */
       end
     end
     else begin
-      if (flip_LRU)
-        LRU[idx] <= ~LRU[idx];
+      // if (flip_LRU)
+      //   LRU[idx] <= ~LRU[idx];
       
       if (cache_write) begin // update cache data and set dirty.
         case (bo)
-          2'b00: DataBank[idx][hit_way][31:0] <= din;
-          2'b01: DataBank[idx][hit_way][63:32] <= din;
-          2'b10: DataBank[idx][hit_way][95:64] <= din;
-          2'b11: DataBank[idx][hit_way][127:96] <= din;
+          2'b00: DataBank[idx][0][31:0] <= din;
+          2'b01: DataBank[idx][0][63:32] <= din;
+          2'b10: DataBank[idx][0][95:64] <= din;
+          2'b11: DataBank[idx][0][127:96] <= din;
         endcase
         
-        is_dirty[idx][hit_way] <= 1;
+        is_dirty[idx][0] <= 1;
       end
 
       if (cache_alloc) begin
-        TagBank[idx][LRU[idx]] <= tag;
-        is_valid[idx][LRU[idx]] <= 1;
-        DataBank[idx][LRU[idx]] <= dout_dmem;
+        TagBank[idx][0] <= tag;
+        is_valid[idx][0] <= 1;
+        DataBank[idx][0] <= dout_dmem;
       end
 
       if (cache_clean) begin
-        TagBank[idx][LRU[idx]] <= 25'b0;
-        is_valid[idx][LRU[idx]] <= 0;
-        is_dirty[idx][LRU[idx]] <= 0;
-        DataBank[idx][LRU[idx]] <= 128'b0;
+        TagBank[idx][0] <= 24'b0;
+        is_valid[idx][0] <= 0;
+        is_dirty[idx][0] <= 0;
+        DataBank[idx][0] <= 128'b0;
       end
     end
   end
